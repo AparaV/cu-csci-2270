6ff4971c91e3dfd9324c516e1fb5bb997327bb5a
Apara <ssapar@gmail.com>
2017-03-06 18:31:41 -0700

    Midterm 2 review

---
+++ Exam-Review/Midterm2-review-inclass.cpp
@@ -0,0 +1,42 @@
+/*
+  Topics
+  ======
+
+  * Linked List (Single and Double)
+      - Traversing a list
+      - Search a list
+      - Add a node to a list
+      - Delete a node from a list
+      - x->next; x->previous
+
+  * Queue (Array and Linke List implementation)
+      - how they're similar and different
+      - Enqueue and Dequeue
+      - how do they change the state of the queue
+      - what is the purpose of checking whether a queue is full or empty
+      - Circular array - state of queue after enequeue and dequeue operations
+
+  * Stack
+      - push and pop
+      - state of stack after a seqeuence of operations
+      - array and linked list implementation
+
+  * BST
+      - Traverse a tree
+      - Add a node to a tree
+      - Search a  tree
+      - Delete a node and identify the replacement
+      - understand pointers for parent and children
+      - no recursion
+
+  Circular queue
+  --------------
+    * Enqueue adds at tail
+    * Dequeue removes at head
+
+  Stack
+  -----
+    * Push - add to stack
+    * Pop - remove from stack
+
+*/
